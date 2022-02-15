37f67aea5fc15bb0331fe082ab8bfeb9b5dcecb9
dpintos <d.pintossi@uva.nl>
2022-02-15 15:24:23 +0100

    Added choice of scenario

--- main.py
+++ main.py
@@ -18,8 +18,13 @@ st.set_page_config(
 st.title('Stoichiometry in Gas-Liquid reactions')
 st.subheader('(in flow)')
 
+# TODO remove when finished
 st.markdown('*This Streamlit app is a work in progress*')
 
-st.button(
-    label='Hoi! Ik ben Benji'
+scenario = st.radio(
+    label='Select the configuration for your Gas-Liquid flow reaction:',
+    options=['Loop filling', 'Continuous flow'],
+    index=0,
+    key='scenario-choice',
+    help='Choose the scenario corresponding to your experimental design.'
 )
