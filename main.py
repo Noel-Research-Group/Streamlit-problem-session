ea209d66a4067f1d12499a34121deae1feccbf3c
dpintos <d.pintossi@uva.nl>
2022-02-15 22:28:54 +0100

    Updated LaTeX code

--- main.py
+++ main.py
@@ -97,12 +97,10 @@ if scenario == 'Loop filling':
         )
 
     with st.expander('How to calculate the solution:'):
-        st.markdown('With the loop filling method, the stoichiometric ratio is simply determined by the ratio of the molar flow rates. Pressure (and volumes) are **not** relevant.')
+        st.markdown('With the loop filling method, the stoichiometric ratio is simply determined by the **ratio of the molar flow rates**. Pressure (and volumes) are **not** relevant.')
         st.latex(
             r'''
-            a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-            \sum_{k=0}^{n-1} ar^k =
-            a \left(\frac{1-r^{n}}{1-r}\right)
+            \dot{n}_{substrate} = c_{substrate} \cdot \dot{v}_{substrate}
             '''
         )
 
