8b95b60bcf4f5cc3fe76fae9eba673321d09d194
dpintos <d.pintossi@uva.nl>
2022-02-15 09:51:54 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Streamlit-based web app for calculations of stoichiometry and stuff in
+gas-liquid reactions.
+
+D.Pintossi
+2022-02-15
+"""
+
+import streamlit as st
+
+st.title('Title')
+
+st.button(
+    label='Hoi! Ik ben Benji'
+)
