3a75726413cd14d074bbadae42cedbd4a94138f7
dpintos <d.pintossi@uva.nl>
2022-02-15 15:34:29 +0100

    Import of gas equation and new GUI elements

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 """
 Streamlit-based web app for calculations of stoichiometry and stuff in
-gas-liquid reactions.
+gas-liquid flow reactions.
 
 D.Pintossi
 2022-02-15
 """
 
 import streamlit as st
+from gas_equations import *
 
 # Streamlit page setup
 st.set_page_config(
@@ -15,8 +16,8 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.title('Stoichiometry in Gas-Liquid reactions')
-st.subheader('(in flow)')
+st.title('Stoichiometry in Gas-Liquid flow reactions')
+st.markdown('*D.Pintossi, February 2022*')
 
 # TODO remove when finished
 st.markdown('*This Streamlit app is a work in progress*')
@@ -28,3 +29,29 @@ scenario = st.radio(
     key='scenario-choice',
     help='Choose the scenario corresponding to your experimental design.'
 )
+
+if scenario == 'Loop filling':
+    with st.sidebar:
+        st.subheader('Experimental parameters:')
+        st.number_input(
+            label='Gas equivalents',
+            min_value=0,
+            max_value=20,
+            value=10,
+            step=1,
+            help='Gas:Substrate stoichiometric ratio',
+            key='equivalents',
+        )
+else:
+    with st.sidebar:
+        st.subheader('Experimental parameters:')
+        st.number_input(
+            label='Gas equivalents',
+            min_value=0,
+            max_value=20,
+            value=10,
+            step=1,
+            help='Gas:Substrate stoichiometric ratio',
+            key='equivalents',
+        )
+        st.slider()
