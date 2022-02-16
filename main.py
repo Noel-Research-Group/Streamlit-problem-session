ec1ea5660ebfeac132f8d5c262f37a261014a29f
dpintos <d.pintossi@uva.nl>
2022-02-16 22:27:03 +0100

    Resized images

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from gas_equations import *
 
 # Streamlit page setup
 st.set_page_config(
-    page_title="Gas-Liquid flow reaction calculator",  # label displayed by the browser
+    page_title="Gas-Liquid flow reaction calculator",
     layout="wide",
     initial_sidebar_state="expanded"
 )
